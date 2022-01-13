43ece44a90628b5851cb3b4a2b2202b741ec3c94
iForced <iforce@2ch.hk>
2022-01-13 12:28:58 +0300

    create contributor entity for db

--- models/models.js
+++ models/models.js
@@ -1,9 +1,11 @@
 import sequelize from '../db.js'
 import {DataTypes} from 'sequelize'
 
+const defaultAvatar = 'https://thumbs.dreamstime.com/b/male-avatar-icon-flat-style-male-user-icon-cartoon-man-avatar-hipster-vector-stock-91462914.jpg'
+
 export const User = sequelize.define('user', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    avatar_url: {type: DataTypes.STRING, defaultValue: 'avatar.example.com'},
+    avatar_url: {type: DataTypes.STRING, defaultValue: defaultAvatar},
     login: {type: DataTypes.STRING, unique: true, allowNull: false},
     password: {type: DataTypes.STRING},
     html_url: {type: DataTypes.STRING, defaultValue: 'user.example.com'},
@@ -17,6 +19,16 @@ export const Repo = sequelize.define('repo', {
     html_url: {type: DataTypes.STRING, defaultValue: 'repo.example.com'},
     owner: {type: DataTypes.STRING, allowNull: false},
 })
+export const Contributor = sequelize.define('contributor', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING, unique: true, allowNull: false},
+})
+export const RepoContributor = sequelize.define('repoContributor', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+})
 
 User.hasMany(Repo)
 Repo.belongsTo(User)
+
+Contributor.belongsToMany(Repo, {through: RepoContributor})
+Repo.belongsToMany(Contributor, {through: RepoContributor})
